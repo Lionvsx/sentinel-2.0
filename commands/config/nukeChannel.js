612201a9cfa1a3b98bb986ce9c716a52045a66de
Lionvsx <51063205+Lionvsx@users.noreply.github.com>
2021-10-08 11:18:34 +0200

    feat: nuke channel command

---
+++ commands/config/nukeChannel.js
@@ -0,0 +1,50 @@
+const { Permissions } = require('discord.js');
+const { sleep } = require('../../utils/functions/utilitaryFunctions');
+const BaseCommand = require('../../utils/structures/BaseCommand')
+
+module.exports = class NukeChannelCommand extends BaseCommand {
+    constructor () {
+        super('nuke', 'config', [], {
+            usage: "nuke",
+            description: "Nuke le channel textuel dans lequel vous lancez la commande",
+            categoryDisplayName: `⚙️ Config`,
+            userPermissions: [Permissions.FLAGS.ADMINISTRATOR],
+            clientPermissions: [],
+            examples: [],
+            serverOnly: true,
+            admin: false,
+            home: false,
+            subCommands: false
+        });
+    }
+
+    async run (bot, message, args) {
+        const loading = bot.emojis.cache.get('741276138319380583')
+
+        message.channel.send(`**${loading} |** Nuke du channel dans 5 secondes`)
+        await sleep(5000)
+
+        if (message.channel.type != 'GUILD_TEXT') return message.channel.send(`**:x: |** Ce channel ne peut pas être nuke !`)
+
+        let channelOptions = {
+            type: 'GUILD_TEXT',
+            topic: message.channel.topic,
+            nsfw: message.channel.nsfw,
+            bitrate: message.channel.bitrate,
+            parent: message.channel.parent,
+            permissionOverwrites: message.channel.permissionOverwrites.cache,
+            position: message.channel.position,
+            rateLimitPerUser: message.channel.rateLimitPerUser,
+            reason: 'Sentinel Channel Nuke'
+        }
+        let channelName = message.channel.name
+        message.channel.delete()
+
+        message.guild.channels.create(channelName, channelOptions).then(newChannel => {
+            newChannel.setPosition(channelOptions.position)
+            newChannel.send(`**:white_check_mark: |** Channel nuked!`)
+        })
+    }
+}
+
+
