63edc1f9dd8f9d84465db9ba6beeaca163eaba7a
Lionvsx <51063205+Lionvsx@users.noreply.github.com>
2021-10-08 12:20:14 +0200

    feat(logs): add log support over reboot

--- events/ready/ready.js
+++ events/ready/ready.js
@@ -2,8 +2,9 @@ const BaseEvent = require('../../utils/structures/BaseEvent');
 
 const Guild = require('../../src/schemas/GuildSchema')
 const User = require('../../src/schemas/UserSchema')
-const Tickets = require('../../src/schemas/TicketSchema')
 const { showCommandLoad } = require('../../utils/register')
+
+const DiscordLogger = require('../../utils/services/discordLoggerService')
 
 require('dotenv').config
 
@@ -19,6 +20,12 @@ module.exports = class ReadyEvent extends BaseEvent {
 
         const commands = []
 
+        const sentinelLogger = new DiscordLogger('sentinel', '#23e5ec')
+        const ldvGuild = client.guilds.cache.get('227470914114158592')
+        sentinelLogger.setLogMember(client)
+        sentinelLogger.setGuild(ldvGuild)
+        const logData = []
+
         for (const [name, interaction] of client.interactions) {
             if (interaction.type === 'slashCommand') commands.push(interaction.help.commandData.toJSON())
         }
@@ -27,20 +34,21 @@ module.exports = class ReadyEvent extends BaseEvent {
             const guildConfig = await Guild.findOne({ guildId: key });
             if (guildConfig) {
                 client.config.set(key, guildConfig)
-                console.log(`Loaded config data for guild : ${value.name}`)
+                console.log(`Loaded config data for guild : ${value.name}`) && logData.push(`Loaded config data for guild : ${value.name}`)
             } else {
                 Guild.create({
                     guildId: key,
                     guildName: value.name
                 }, async (err) => {
-                    if (err) throw err && console.log(`There was an error trying to save GUILD : ${value.name} to the database !`)
-                    else console.error(`⚠️ Guild : ${value.name} wasn't saved in the database, created new entry ! ⚠️`)
+                    if (err) throw err && console.log(`There was an error trying to save GUILD : ${value.name} to the database !`) && logData.push(`There was an error trying to save GUILD : ${value.name} to the database !`)
+                    else console.error(`⚠️ Guild : ${value.name} wasn't saved in the database, created new entry ! ⚠️`) && logData.push(`⚠️ Guild : ${value.name} wasn't saved in the database, created new entry ! ⚠️`)
                 }) 
             }
         }
         
     
         console.log('Started refreshing application (/) commands.');
+        logData.push('Started refreshing application (/) commands.');
         for (const [key, value] of client.guilds.cache) {
             const guild = await client.guilds.cache.get(key)
             const guildConfig = client.config.get(key)
@@ -52,6 +60,7 @@ module.exports = class ReadyEvent extends BaseEvent {
                         try {
                             client.application.commands.set(commands, guild.id)
                             console.log(`Loaded ${commands.length} (/) commands for guild ${guild.name}`)
+                            logData.push(`Loaded ${commands.length} (/) commands for guild ${guild.name}`)
                         } catch (err) {
                             console.error(err)
                         }
@@ -61,6 +70,7 @@ module.exports = class ReadyEvent extends BaseEvent {
             }
         }
         console.log('Successfully reloaded application (/) commands.');
+        logData.push('Successfully reloaded application (/) commands.')
         const Users = await User.find({ onServer: true })
         for (const user of Users) {
             client.allUsers.set(user.discordId, user)
@@ -70,7 +80,11 @@ module.exports = class ReadyEvent extends BaseEvent {
         //     client.allTickets.set(ticket.ticketChannelId, ticket)
         // }
 
-        console.log(`Cached Users : ${client.allUsers.size}`)
+        console.log(`Cached Users on guild ${ldvGuild.name}: ${client.allUsers.size}`)
+        logData.push(`Cached Users on guild ${ldvGuild.name}: ${client.allUsers.size}`)
+
+        sentinelLogger.setLogData(logData.join('\n'))
+        sentinelLogger.info(`**BOT ${client.user.username} REBOOTED AND READY :**`)
 
         
 
