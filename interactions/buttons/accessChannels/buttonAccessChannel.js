7f379ddd6a85254405d2e90b05d873e66a613635
Lionvsx <51063205+Lionvsx@users.noreply.github.com>
2021-10-12 19:41:19 +0200

    feat(tickets): add access channels in categories

---
+++ interactions/buttons/accessChannels/buttonAccessChannel.js
@@ -0,0 +1,52 @@
+const BaseInteraction = require('../../../utils/structures/BaseInteraction')
+const DiscordLogger = require('../../../utils/services/discordLoggerService')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = class AccessChannelButton extends BaseInteraction {
+    constructor() {
+        super('buttonAccessChannel', 'dashboards', 'button', {
+            userPermissions: [],
+            clientPermissions: []
+        })
+    }
+
+    async run(client, interaction, buttonArgs) {
+        if (!buttonArgs[1]) return
+        const allChannels = interaction.guild.channels.cache;
+        const accessChannel = allChannels.get(buttonArgs[1]);
+
+        const ticketLogger = new DiscordLogger('tickets', '#ffeaa7')
+        ticketLogger.setGuild(interaction.guild)
+        ticketLogger.setLogMember(interaction.member)
+
+        if (!accessChannel) {
+            const embed = new MessageEmbed()
+                .setDescription("Ce ticket n'existe plus !")
+                .setColor('#e74c3c')
+
+            interaction.update({
+                embeds: [embed],
+                components: []
+            })
+            return;
+        }
+
+        try {
+            accessChannel.permissionOverwrites.create(interaction.user, {
+                SEND_MESSAGES: true,
+                VIEW_CHANNEL: true
+            })
+            ticketLogger.info(`<@!${interaction.user.id}> a accédé au ticket \`${accessChannel.name}\``)
+            accessChannel.send({
+                content: `**➡️ | **\`${interaction.user.username}\` a rejoint le ticket !`
+            })
+            interaction.reply({
+                content: `Accès authorisé au ticket ${accessChannel.name}`,
+                ephemeral: true
+            })
+        } catch (err) {
+            console.log(err)
+            ticketLogger.error(`<@!${interaction.user.id}> n'a pas pu accéder au ticket avec l'id \`${buttonArgs[1]}\``)
+        }
+    }
+}
---
+++ interactions/buttons/accessChannels/buttonKillAccessChannel.js
@@ -0,0 +1,29 @@
+const BaseInteraction = require('../../../utils/structures/BaseInteraction')
+const DiscordLogger = require('../../../utils/services/discordLoggerService')
+const { Permissions, MessageEmbed } = require('discord.js')
+
+module.exports = class AccessCategoryButtonInteraction extends BaseInteraction {
+    constructor() {
+        super('buttonKillAccessChannel', 'dashboards', 'button', {
+            userPermissions: [Permissions.FLAGS.KICK_MEMBERS],
+            clientPermissions: []
+        })
+    }
+
+    async run(client, interaction, buttonArgs) {
+
+        const ticketLogger = new DiscordLogger('tickets', '#ffeaa7')
+        ticketLogger.setGuild(interaction.guild)
+        ticketLogger.setLogMember(interaction.member)
+
+        const embed = new MessageEmbed()
+            .setDescription("Accès supprimé par le Responsable")
+            .setColor('#e74c3c')
+
+        interaction.update({
+            embeds: [embed],
+            components: []
+        })
+        ticketLogger.info(`Accès à un ticket fermé par <@!${interaction.user.id}>`)
+    }
+}
