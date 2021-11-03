ffd0f68b1c568647716e6a5001d72d2defc7eb25
Lionvsx <51063205+Lionvsx@users.noreply.github.com>
2021-11-03 16:48:08 +0100

    feat: access category bureau dashboard

---
+++ interactions/buttons/dashboards/teams/buttonStartCall.js
@@ -0,0 +1,19 @@
+const BaseInteraction = require('../../../../utils/structures/BaseInteraction')
+const { Permissions } = require('discord.js')
+
+module.exports = class ButtonStartCall extends BaseInteraction {
+    constructor() {
+        super('buttonStartCall', 'dashboards', 'button', {
+            userPermissions: [],
+            clientPermissions: []
+        })
+    }
+
+    async run(client, interaction, buttonArgs) {
+        interaction.reply({
+            content: `L'appel a été ouvert !`,
+            ephemeral: true
+        })
+        
+    }
+}
