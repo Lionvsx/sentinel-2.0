f6971da83f075d851357a1987032c17244af8ebf
Lionvsx <51063205+Lionvsx@users.noreply.github.com>
2021-11-03 16:46:07 +0100

    fix: externe firing as not members

--- utils/functions/dbFunctions.js
+++ utils/functions/dbFunctions.js
@@ -3,6 +3,6 @@ module.exports = {
 }
 
 function isMember(dBUser) {
-    if (dBUser.firstName && dBUser.lastName && dBUser.school) return true;
+    if (dBUser.firstName && dBUser.lastName && dBUser.school && dBUser.isMember) return true;
     else return false;
 }
