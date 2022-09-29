4282806c15ce31df8573ca97275cef426d095c75
Lionvsx <lionvsx@outlook.com>
2022-09-29 15:27:35 +0200

    feat: add logging in structures

--- utils/structures/BaseEvent.js
+++ utils/structures/BaseEvent.js
@@ -1,5 +1,33 @@
+const Logger = require("../services/logger");
+
 module.exports = class BaseEvent {
     constructor(name) {
         this.name = name;
+        this.consoleLogger = new Logger(name);
+    }
+
+    /**
+     *
+     * @param message {String}
+     * @param logData {JSON}
+     */
+    log(message, logData = undefined) {
+        logData ? this.consoleLogger.log(message, 'info') : this.consoleLogger.log(message, 'info', logData);
+    }
+    /**
+     *
+     * @param message {String}
+     * @param logData {JSON}
+     */
+    error(message, logData = undefined) {
+        logData ? this.consoleLogger.log(message, 'error') : this.consoleLogger.log(message, 'error', logData);
+    }
+    /**
+     *
+     * @param message {String}
+     * @param logData {JSON}
+     */
+    warn(message, logData = undefined) {
+        logData ? this.consoleLogger.log(message, 'warn') : this.consoleLogger.log(message, 'warning', logData);
     }
 }
--- utils/structures/BaseInteraction.js
+++ utils/structures/BaseInteraction.js
@@ -1,8 +1,36 @@
+const Logger = require('../services/logger')
+
 module.exports = class BaseInteraction {
     constructor(name, category, type, help) {
         this.name = name;
         this.category = category;
         this.type = type;
         this.help = help;
+        this.consoleLogger = new Logger(name);
+    }
+
+    /**
+     *
+     * @param message {String}
+     * @param logData {JSON}
+     */
+    log(message, logData = undefined) {
+        logData ? this.consoleLogger.log(message, 'info') : this.consoleLogger.log(message, 'info', logData);
+    }
+    /**
+     *
+     * @param message {String}
+     * @param logData {JSON}
+     */
+    error(message, logData = undefined) {
+        logData ? this.consoleLogger.log(message, 'error') : this.consoleLogger.log(message, 'error', logData);
+    }
+    /**
+     *
+     * @param message {String}
+     * @param logData {JSON}
+     */
+    warn(message, logData = undefined) {
+        logData ? this.consoleLogger.log(message, 'warn') : this.consoleLogger.log(message, 'warning', logData);
     }
 }
