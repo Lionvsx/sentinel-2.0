b12e2b7bd1611446d0e7825676d46d28c196f22a
Leonard Roussard <lroussard@gmail.com>
2023-09-25 18:41:01 +0200

    fix: Smart manager staff interface join embed bug

--- events/message/messageCreate_SM.js
+++ events/message/messageCreate_SM.js
@@ -1,7 +1,13 @@
 const BaseEvent = require('../../utils/structures/BaseEvent');
 const Teams = require('../../src/schemas/TeamSchema');
+const {minutesToHHMM, getParisUTCOffset} = require("../../utils/functions/systemFunctions");
+const OpenAIInterface = require("../../ai/OpenAIInterface");
 const {MessageEmbed} = require("discord.js");
-const {getDateTime} = require("../../utils/functions/systemFunctions");
+const {
+    createEmojiButton,
+    createMessageActionRow
+} = require("../../utils/functions/messageComponents");
+const {Types} = require("mongoose");
 
 module.exports = class MessageCreateSmartManagerEvent extends BaseEvent {
     constructor() {
@@ -15,75 +21,164 @@ module.exports = class MessageCreateSmartManagerEvent extends BaseEvent {
 
         if (message.channel.name !== '📌┃organisation') return;
         let parentChannelID = message.channel.parent.id;
-        let Team = Teams.findOne({linkedCategoryId: parentChannelID});
+        let Team = await Teams.findOne({linkedCategoryId: parentChannelID});
 
         if (!Team || !Team.smartManager) return;
 
-        let response = await client.openAIAgent.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [
-                { role: "system", content: "You are an AI Agent designed to transform the announcement message into a smart message using the provided function. Only use the function when there is an event in the user message\n Current time and date is " + getDateTime()},
-            ],
-            temperature: 0,
-            functions: [
-                {
-                    name: "create-event",
-                    description: "Create an event for an esport team",
-                    parameters: {
-                        type: "object",
-                        properties: {
-                            eventType: {
-                                type: "string",
-                                enum: ["training", "scrim", "tournament", "info", "pracc"]
-                            },
-                            eventName: {
-                                type: "string",
-                            },
-                            eventDate: {
-                                type: "string",
-                                description: "Date of the event in the format: 2021-09-30T18:30:00.000Z"
-                            },
-                            eventDescription: {
-                                type: "string",
-                            },
-                            eventLink: {
-                                type: "string",
-                                description: "Useful link for the event, or tracker if it's a pracc / scrim"
-                            },
-                            needRSVP: {
-                                type: "boolean",
-                                description: "Whether or not the event needs an RSVP from the team members"
-                            },
-                            embed: {
-                                type: "object",
-                                properties: {
-                                    "title": {
+        const currentDate = new Date(Date.now());
+        const options = { weekday: 'long' };
+        const day = currentDate.toLocaleDateString('en-US', options);
+
+        await message.react(client.loadingEmoji)
+
+        const Interface = new OpenAIInterface(client, "You are an AI Agent designed to transform the announcement message into a smart message using the provided function. Only use the function when there is an event in the user message." +
+            "Current time and date is " + currentDate.toISOString() + " and we are " + day +
+            "The duration for 1 game is " + Team.trainingTime + " minutes.")
+
+        let trainTags = Team.trainTags.concat(["team-building", "review", "tournament"]);
+
+        let response = await Interface.callGPT('gpt-4', [
+            {
+                "name": 'create-events',
+                "description": "Create events and training sessions for an esports team based on player availability.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "events": {
+                            "type": "array",
+                            "items": {
+                                "type": "object",
+                                "properties": {
+                                    "eventType": {
                                         "type": "string",
-                                        "description": "The title of the embed, should be in this format : emoji ` Event name `"
+                                        "enum": trainTags,
+                                        "description": "Type of event to be created."
                                     },
-                                    "description": {
+                                    "date": {
                                         "type": "string",
-                                        "description": "The description of the embed"
+                                        "format": "yyyy-MM-ddTHH:mm:ss.sssZ",
+                                        "description": "Start date and time of the event. Follow ISO 8601 Format for dates."
+                                    },
+                                    "duration": {
+                                        "type": "number",
+                                        "description": "Duration of the event in minutes. If there is games refer to the duration for 1 game"
+                                    },
+                                    "numberOfGames": {
+                                        "type": "number",
+                                        "description": "Number of games for a " + Team.trainTags.join(' or ') + "0 for ANY OTHER TYPE"
                                     }
                                 },
-                                required: ["title", "description"]
+                                "required": ["eventType", "date", "duration"]
                             }
-                        },
-                        required: ["eventType", "eventName", "eventDate", "eventDescription", "needRSVP", "embed"]
-                    }
+                        }
+                    },
+                    "required": ["events"]
                 }
-            ],
-            function_call: "auto",
-        })
+            }
+        ], message.content)
 
-        if (response.data.choices[0].function_call) {
-            let params = JSON.parse(response.data.choices[0].function_call.arguments)
+        let organisationChannel = message.channel.parent.children.find(channel => channel.name.includes('organisation'))
+
+        if (response.finish_reason !== "function_call") {
+            message.reply({
+                content: `<:x_:1137419292946727042> Erreur : ${response.message.content}`
+            })
+            return;
+        }
+
+        let responseData = JSON.parse(response.message.function_call.arguments)
+        for (const event of responseData.events) {
+
+            // Switch emoji on event type :
+            let title = ''
+            switch (event.eventType) {
+                case 'training':
+                    title = '<:zap:1137424324144410736> ` TRAINING `'
+                    break
+                case 'pracc':
+                    title = '<:crosshair:1137436482248904846> ` PRACC `'
+                    break
+                case 'tournament':
+                    title = '<:flag:1153289152536772659> ` TOURNAMENT `'
+                    break
+                case 'scrim':
+                    title = '<:zap2:1137424322399571988> ` SCRIM `'
+                    break
+                case 'team-building':
+                    title = '<:users:1137390672194850887> ` TEAM BUILDING `'
+                    break
+                case 'review':
+                    title = '<:search:1153289155405680721> ` REVIEW `'
+                    break
+                default:
+                    title = '<:calendar:1137424147056689293> ` EVENT `'
+                    break
+            }
+            let offset = getParisUTCOffset() // offset in hours between UTC and paris
+            const unixTimestamp = Math.floor(new Date(event.date).getTime() / 1000) - (offset * 3600);
+
+            let embedDescription = `<:calendar:1137424147056689293> \` DATE \` <t:${unixTimestamp}:F>\n<:clock:1139536765837901916> \` DURÉE \` ${minutesToHHMM(event.duration)}\n`
+
+            if (event.eventType !== 'review' && event.eventType !== 'team-building') {
+                embedDescription += `<:arrowrightcircle:1137421115766083726> \` GAMES \` ${event.numberOfGames}\n`
+            }
+            embedDescription += `<:pluscircle:1137390650690650172> \` JOINED \` 0/${Team.minPlayers}`
+
 
             let eventEmbed = new MessageEmbed()
-                .setTitle(params.embed.title)
-                .setDescription(params.embed.description)
+                .setTitle(title)
                 .setColor('#2b2d31')
-                .setImage("https://cdn.discordapp.com/attachments/1133094075625640167/1133367506300571719/1440x1-00ffff7f.png")
+                .setDescription(embedDescription)
+
+            const myId = Types.ObjectId();
+
+
+            let buttonAccept = createEmojiButton(`acceptEvent|${myId}`, '', 'SECONDARY', '<:usercheck:1137390666490589274>')
+            let buttonMaybe = createEmojiButton(`maybeEvent|${myId}`, '', 'SECONDARY', '<:userplus3:1153405260812005547>')
+            let buttonDecline = createEmojiButton(`declineEvent|${myId}`, '', 'SECONDARY', '<:userx:1137394869812351006>')
+            let buttonSettings = createEmojiButton(`eventSettings|${myId}`, '', 'SECONDARY', '<:settings2:1153405967409623141>')
+
+
+            eventEmbed.addFields([
+                {
+                    name: '<:check:1137390614296678421> ` CONFIRMES `',
+                    value: '\u200b',
+                    inline: true
+                },
+                {
+                    name: '<:users:1137390672194850887> ` PEUT-ÊTRE `',
+                    value: '\u200b',
+                    inline: true
+                },
+                {
+                    name: '<:x_:1137419292946727042> ` INDISPONIBLES `',
+                    value: '\u200b',
+                    inline: true
+                }
+            ])
+            let eventMessage = await organisationChannel.send({
+                content: "<@&" + Team.linkedRoleId + ">",
+                embeds: [eventEmbed],
+                components: [
+                    createMessageActionRow([buttonAccept, buttonMaybe, buttonDecline, buttonSettings])
+                ]
+            })
+
+
+            Team.events.push({
+                _id: myId,
+                name: title,
+                type: event.eventType,
+                attendance: true,
+                discordTimestamp: unixTimestamp,
+                duration: event.duration,
+                nbGames: event.numberOfGames,
+                slots: Team.minPlayers,
+                messageId: eventMessage.id
+            })
+            await Team.save()
         }
+
+        message.delete()
     }
 }
